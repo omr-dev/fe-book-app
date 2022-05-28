e87a1b8884107b9d8d4499be1bd16f486b4230c1
omer <peynirciomerfaruk@gmail.com>
2022-05-28 17:34:49 +0200

    spinner and fade in added

--- src/subPages/PageBooks.jsx
+++ src/subPages/PageBooks.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { FaSpinner } from "react-icons/fa";
+import FadeIn from "react-fade-in";
 import { Book } from "../components/Book.jsx";
 
 const apiUrl = "https://server-book-app.herokuapp.com";
@@ -18,16 +19,18 @@ export const PageBooks = () => {
     <div className="pageBooks">
       {books.length === 0 ? (
         <>
-          <div className="spinner">
-            <FaSpinner />
+          <div>
+            <FaSpinner className="spinner" />
           </div>
         </>
       ) : (
         <h3>Total {books.length} books.</h3>
       )}
-      {books.map((book, index) => (
-        <Book key={index} book={book} />
-      ))}
+      <FadeIn>
+        {books.map((book, index) => (
+          <Book key={index} book={book} />
+        ))}
+      </FadeIn>
     </div>
   );
 };
