cd958ebd78f6fd7bb404717fbbeb8cbde1b26470
omer <peynirciomerfaruk@gmail.com>
2022-05-28 16:50:15 +0200

    api connected

--- src/subPages/PageBooks.jsx
+++ src/subPages/PageBooks.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const apiUrl = "https://server-book-app.herokuapp.com";
+
 export const PageBooks = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const _books = (await axios.get(apiUrl)).data;
+      setBooks(_books);
+    })();
+  }, []);
   return (
     <div className="pageBooks">
-      <h2>This is page Books</h2>
+      <h2>This is page Books.</h2>
+      <h3>Total {books.length} books.</h3>
     </div>
   );
 };
